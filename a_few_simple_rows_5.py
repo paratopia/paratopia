0acc57b2dadef83f53c7077d7fc101a1d3807f4b
paratopia <79787827+paratopia@users.noreply.github.com>
2022-07-16 08:37:25 +0200

    Update a_few_simple_rows_5.py

--- a_few_simple_rows_5.py
+++ a_few_simple_rows_5.py
@@ -4,8 +4,7 @@ gam = 5
 def theta(t): 
     tet = np.array([t[0], t[1]])
     w = list(map(lambda i:(1/N)*(1+np.dot(xhat[i].T, tet)), range(len(adj))))
-    pr = list(map(lambda i:np.dot(np.exp(np.log(w[i])), ret[i]),                                      
-    range(len(adj))))
+    pr = list(map(lambda i:np.dot(np.exp(np.log(w[i])), ret[i]), range(len(adj))))
     u = list(map(lambda i:((1+pr[i])**(1-gam))/(1-gam), range(len(adj))))
     return -np.mean(u)
 t0 = 2*[0/2]
