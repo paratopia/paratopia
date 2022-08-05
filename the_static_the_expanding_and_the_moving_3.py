b0d6ce07262c073631bbba094933452ab0ef1c58
paratopia <79787827+paratopia@users.noreply.github.com>
2022-08-05 12:54:42 +0200

    Create the_static_the_expanding_and_the_moving_3.py

---
+++ the_static_the_expanding_and_the_moving_3.py
@@ -0,0 +1,5 @@
+tetb = []
+for window in range (k2, len(inin)):
+  inin1, N1, r1, xhat1 = inin[:window], N[:window], r[:window], xhat[:window]
+  res = minimize(theta, t0, method="Nelder-Mead", tol=1.e-12)
+  tetb.append(res.x) 
