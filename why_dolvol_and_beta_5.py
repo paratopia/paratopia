8758fb6171a8de785a292e0f31b7626fbf57f1d0
paratopia <79787827+paratopia@users.noreply.github.com>
2022-08-22 13:40:35 +0200

    Update why_dolvol_and_beta_5.py

--- why_dolvol_and_beta_5.py
+++ why_dolvol_and_beta_5.py
@@ -5,3 +5,7 @@ rdolbet = list(map(lambda i: np.dot(wdolbet[i], r2[i]), range(len(inin2))))
 rdolmom = list(map(lambda i: np.dot(wdolmom[i], r2[i]), range(len(inin2))))
 rbetmom = list(map(lambda i: np.dot(wbetmom[i], r2[i]), range(len(inin2))))
 rdolbetmom = list(map(lambda i: np.dot(wdolbetmom[i], r2[i]), range(len(inin2))))
+
+rfr = pd.read_csv("rfppp.csv", delimiter=";", index_col="Date").values  
+rfr = rfr[len(rfr)-len(rdol):]      
+rdol, rbet, rmom, rdolbet, rdolmom, rbetmom, rdolbetmom = rdol-rfr.T, rbet-rfr.T, rmom-rfr.T, rdolbet-rfr.T, rdolmom-rfr.T, rbetmom-rfr.T, rdolbetmom-rfr.T
