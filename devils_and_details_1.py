9331331fa03d413f297bb66bada08a7a9c6ccf88
paratopia <79787827+paratopia@users.noreply.github.com>
2023-02-22 13:37:50 +0100

    Create devils_and_details_1.py

---
+++ devils_and_details_1.py
@@ -0,0 +1,21 @@
+# model B
+def gas_b(p,returnme=False): 
+    omega = np.array([p[0],p[3]])
+    beta = np.array([p[1],p[4]]) 
+    alpha = np.array([p[2],p[5]])   
+    tets = np.zeros((len(inin1),2))
+    tet = omega
+    for i in range(0,len(inin1)):
+        tets[i,:] =  tet
+        S = dj(tet,i)
+        tet = omega+(tet-omega)*beta+alpha*S
+    w = list(map(lambda i: (1/N1[i])*(1+np.dot(x1[i].T,tets[i,:])),range(len(inin1))))
+    pr = list(map(lambda i: np.dot(w[i],r1[i]),range(len(inin1))))
+    u = list(map(lambda i: ((1+pr[i])**(1-rho))/(1-rho),range(len(inin1))))
+    if returnme == True:
+        return tets
+    return -np.mean(u)
+
+bnds = ((-np.inf,np.inf),(0,1),(0,100),(-np.inf,np.inf),(0,1),(0,100))
+x0 = [0.41,0.98,10,0.88,0.98,10]
+res_b = minimize(gas_b,x0,method="Nelder-Mead",bounds=bnds)
